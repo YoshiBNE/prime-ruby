fb39e3661f0eacf03cbc5f15a520eaae0ef2f49d
YoshiBNE <yoshiyasu_express@me.com>
2022-07-04 02:03:12 +1000

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+    if integer == 2
+        return true
+    elsif integer < 2
+        return false
+    else
+        count = integer-1
+        while count > 1
+            if (integer % count == 0)
+                return false
+            end
+            count -= 1
+        end
+        return true
+    end
+end
